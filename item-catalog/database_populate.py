a9be56eb9d92b10efdbb012f48b0d77378c81823
ahough <ahough@godaddy.com>
2018-08-28 11:24:39 -0700

    updated db and db populate script

--- item-catalog/database_populate.py
+++ item-catalog/database_populate.py
@@ -52,54 +52,54 @@ session.commit()
 
 
 #Populate Items
-cleats = Item(name = "cleats", description = "Maintain control of the ball with these padded cleats for extra comfort", category_name = "soccer", created_by_user = "102017630495928835049")
-soccerball = Item(name = "soccer ball", description = "10 inch ball for the best in control", category_name = "soccer", created_by_user = "102017630495928835050")
+cleats = Item(name = "cleats", description = "Maintain control of the ball with these padded cleats for extra comfort", category_id = 1, created_by_user = "102017630495928835049")
+soccerball = Item(name = "soccer ball", description = "10 inch ball for the best in control", category_id = 1, created_by_user = "102017630495928835050")
 session.add(cleats)
 session.add(soccerball)
 
-hoop = Item(name = "basketball hoop", description = "Portable hoop with 60 inch backboard", category_name = "basketball", created_by_user = "102017630495928835049")
-basketball = Item(name = "basketball", description = "Youth 28.5 inch ball", category_name = "basketball", created_by_user = "102017630495928835049")
+hoop = Item(name = "basketball hoop", description = "Portable hoop with 60 inch backboard", category_id = 2, created_by_user = "102017630495928835049")
+basketball = Item(name = "basketball", description = "Youth 28.5 inch ball", category_id = 2, created_by_user = "102017630495928835049")
 session.add(hoop)
 session.add(basketball)
 
-baseball = Item(name = "baseball", description = "9 inch hollow core ball", category_name = "baseball", created_by_user = "102017630495928835049")
-bat = Item(name = "bat", description = "Maple wood composite bat", category_name = "baseball", created_by_user = "102017630495928835049")
+baseball = Item(name = "baseball", description = "9 inch hollow core ball", category_id = 3, created_by_user = "102017630495928835049")
+bat = Item(name = "bat", description = "Maple wood composite bat", category_id = 3, created_by_user = "102017630495928835049")
 session.add(baseball)
 session.add(bat)
 
 session.commit()
 
 
-frisbee = Item(name = "frisbee", description = "175 gram Ultimate frisbee", category_name = "frisbee", created_by_user = "102017630495928835049")
-frisbee_glove = Item(name = "frisbee glove", description = "Gloves for handling and catching", category_name = "frisbee", created_by_user = "102017630495928835049")
+frisbee = Item(name = "frisbee", description = "175 gram Ultimate frisbee", category_id = 4, created_by_user = "102017630495928835049")
+frisbee_glove = Item(name = "frisbee glove", description = "Gloves for handling and catching", category_id = 4, created_by_user = "102017630495928835049")
 session.add(frisbee)
 session.add(frisbee_glove)
 
-snowboard = Item(name = "snowboard", description = "Womens 147cm board", category_name = "snowboarding", created_by_user = "102017630495928835049")
-goggles = Item(name = "goggles", description = "Womens anti-fog goggles with mirrored lenses", category_name = "snowboarding", created_by_user = "102017630495928835049")
+snowboard = Item(name = "snowboard", description = "Womens 147cm board", category_id = 5, created_by_user = "102017630495928835049")
+goggles = Item(name = "goggles", description = "Womens anti-fog goggles with mirrored lenses", category_id = 5, created_by_user = "102017630495928835049")
 session.add(snowboard)
 session.add(goggles)
 
-chalk = Item(name = "hand chalk", description = "Chalk to improve your grip", category_name = "climbing", created_by_user = "102017630495928835049")
-rope = Item(name = "rope", description = "Dynamic rope for maximum elasticity", category_name = "climbing", created_by_user = "102017630495928835049")
+chalk = Item(name = "hand chalk", description = "Chalk to improve your grip", category_id = 6, created_by_user = "102017630495928835049")
+rope = Item(name = "rope", description = "Dynamic rope for maximum elasticity", category_id = 6, created_by_user = "102017630495928835049")
 session.add(chalk)
 session.add(rope)
 
 session.commit()
 
 
-football = Item(name = "football", description = "Laceless training football", category_name = "football", created_by_user = "102017630495928835049")
-tackling_dummy = Item(name = "tackling dummy", description = "Moving football tackling dummy", category_name = "football", created_by_user = "102017630495928835049")
+football = Item(name = "football", description = "Laceless training football", category_id = 7, created_by_user = "102017630495928835049")
+tackling_dummy = Item(name = "tackling dummy", description = "Moving football tackling dummy", category_id = 7, created_by_user = "102017630495928835049")
 session.add(football)
 session.add(tackling_dummy)
 
-blade_covers = Item(name = "blade covers", description = "Protect your skate blades off of the ice", category_name = "skating", created_by_user = "102017630495928835050")
-skates = Item(name = "skates", description = "Youth Freestyle Skates", category_name = "skating", created_by_user = "102017630495928835049")
+blade_covers = Item(name = "blade covers", description = "Protect your skate blades off of the ice", category_id = 8, created_by_user = "102017630495928835050")
+skates = Item(name = "skates", description = "Youth Freestyle Skates", category_id = 8, created_by_user = "102017630495928835049")
 session.add(blade_covers)
 session.add(skates)
 
-helmet = Item(name = "helmet", description = "Four piece shell and liner", category_name = "hockey", created_by_user = "102017630495928835049")
-mouth_guard = Item(name = "mouth guard", description = "Green mouthguard with strap", category_name = "hockey", created_by_user = "102017630495928835049")
+helmet = Item(name = "helmet", description = "Four piece shell and liner", category_id = 9, created_by_user = "102017630495928835049")
+mouth_guard = Item(name = "mouth guard", description = "Green mouthguard with strap", category_id = 9, created_by_user = "102017630495928835049")
 session.add(helmet)
 session.add(mouth_guard)
 
