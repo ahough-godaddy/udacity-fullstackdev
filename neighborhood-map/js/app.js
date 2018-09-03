2d61d774b86db1a4e2fc2be576088f6cb2d38d55
ahough <ahough@godaddy.com>
2018-09-02 20:40:37 -0700

    showing markers and info window

--- neighborhood-map/js/app.js
+++ neighborhood-map/js/app.js
@@ -2,78 +2,144 @@
 Model -- could also load this from a server
 */
 
-var mapLocales = [
-	{
-		clickCount: 0,
-		name: 'Tabby',
-		imgSrc: 'img/434164568_fea0ad4013_z.jpg',
-		imgAttribution: 'https://www.flickr.com/photos/bigtallguy/434164568',
-		nicknames: [
-			{nickname: "Tabitha"},
-			{nickname: "Mrs T"},
-			{nickname: "Little Meow Meow Face"}
-		]	
+var map;
+var $mapElem = $('#map');
+
+var model = {
+	selectedPlace: null,
+	tahoePlaces: [
+		{
+			name: 'Kings Beach',
+			latitude: 39.237984000545,
+			longitude: -120.031498
+		},
+		{
+			name: 'Gar Woods Grill & Pier',
+			latitude: 39.2253,
+			longitude: -120.0836	
+		},
+		{
+			name: 'Safeway',
+			latitude: 39.239526,
+			longitude: -120.034195	
+		},
+		{
+			name: 'Tahoe Treetop Adventure Park',
+			latitude: 39.156743,
+			longitude: -120.152062	
+		},
+		{
+			name: 'Kings Beach Jet Ski Rentals',
+			latitude: 39.237465,
+			longitude: -120.026709	
+		}
+
+	]
+};
+
+var viewModel = {
+
+	init: function() {
+
+		
+		markersView.init();
+		markersListView.init();
+
 	},
-	{
-		clickCount: 0,
-		name: 'Tiger',
-		imgSrc: 'img/4154543904_6e2428c421_z.jpg',
-		imgAttribution: 'https://www.flickr.com/photos/xshamx/4154543904',
-		nicknames: [
-			{nickname: "Tigger"},
-			{nickname: "Mr Growls"}
-		]
+
+	getPlaces: function() {
+		console.log("Getting map markers " + model.tahoePlaces.length);
+		return model.tahoePlaces;
+	},
+
+	setSelectedPlace: function(place) {
+		console.log("selected place: " + place.name)
+		model.selectedPlace = place;
+	},
+
+	getSelectedPlace: function() {
+		return model.selectedPlace;
+	}
+}
+
+
+
+var mapView = {
+
+	init: function() {
+
+		var mapsTimeout = setTimeout(function() {
+        	alert("Failed to load Google Maps resources within allowed timeframe");
+    	}, 8000);
+
+		map = new google.maps.Map(document.getElementById('map'), {
+              center: {lat: 39.237984000545, lng: -120.031498},
+              zoom: 12
+            });
+
+		if ( $mapElem.length ) {
+			clearTimeout(mapsTimeout);
+		}
+
+		viewModel.init();
+		
 	}
 
-]
-
-var ViewModel = function() { 
-	var self = this;
-
-	self.setmap = function() {
-
-	};
-
-	this.catList = ko.observableArray([]);
-
-	mapLocales.forEach(function(catItem) {
-		self.catList.push(new Cat(catItem));
-	});
-
-	this.currentCat = ko.observable(this.catList()[0]);
-
-	this.incrementCounter = function() {
-		self.currentCat().clickCount(self.currentCat().clickCount() + 1);
-	};
-
-	this.setCat = function(clickedCat) {
-		self.currentCat(clickedCat);
-	};
-}
-
-
-
-var Cat = function(data) {
-	this.clickCount = ko.observable(data.clickCount);
-	this.name = ko.observable(data.name);
-	this.imgSrc = ko.observable(data.imgSrc);
-	this.imgAttribution = ko.observable(data.imgAttribution);
-	this.nicknames = ko.observableArray(data.nicknames);	
-
-	this.level = ko.computed(function() {
-		if (this.clickCount() < 5) {
-			return "kitten";
-		}
-		else if (this.clickCount() < 10) {
-			return "teen";
-		}
-		else if (this.clickCount() < 34) {
-			return "cat";
-		}
-		else {
-			return "grumpy old man";
-		}
-	}, this);
 
 }
 
+var markersView = {
+
+	init: function() {
+
+		this.render();
+
+	},
+
+	render: function() {
+		var places = viewModel.getPlaces();
+
+		for (i = 0; i < places.length; i++) {
+			var place = places[i];
+			var marker = new google.maps.Marker({
+		      position: {lat: place.latitude, lng: place.longitude},
+		      map: map,
+		      animation: google.maps.Animation.DROP,
+		      title: place.name
+		    });
+
+		    marker.addListener('click', (function(placeCopy) {
+		    	return function() {
+		    		viewModel.setSelectedPlace(placeCopy);
+		    		showInfoWindow.render();
+		    	};
+		    })(place));
+		}
+	}
+
+}
+
+var markersListView = {
+
+	init: function() {
+
+	}
+
+}
+
+var showInfoWindow = {
+	render: function() {
+		var selectedPlace = viewModel.getSelectedPlace();
+		console.log("Showing info window for : " + selectedPlace.name);
+
+		//Call Yelp to get information and use that to create the InfoWindow!!!!!!!
+
+		var infowindow = new google.maps.InfoWindow({
+			content: "here I am " + selectedPlace.name,
+			position: {lat: selectedPlace.latitude, lng: selectedPlace.longitude}
+		});
+
+		infowindow.open(map);
+	}
+
+}
